b99919281d971f8eb68a4927c107b8e08a2abced
TBLGsn <1844760779@qq.com>
2021-09-17 17:02:18 +0800

    update

--- bin/menu.sh
+++ bin/menu.sh
@@ -2,7 +2,7 @@
 # 0. 安装服务器
 install() {
     chmod u+x installdst.sh # 添加运行权限
-   ./installdst.sh 
+    bash installdst.sh 
 }
 # 1. 当前服务器状态（测试 所有服务器）
 verity_status(){
@@ -14,24 +14,37 @@ verity_status(){
 update() {
     cd /home/dst/server_dst/bin || Printf_RED "设置定时任务出错"
 }
+Printf_Green(){
+    echo -e "\033[42;37m $* \033[0m"
+}
 # 3. 启动服务器
 start_server() {
     cd /home/dst/server_dst/bin/
     nohup ./start.sh  >/dev/null 2>&1 &
     nohup ./start2.sh  >/dev/null 2>&1 &
+    Printf_Green "启动成功,5s 后返回主菜单"
+    sleep 5
 }
 # 3. 停止服务器
 stop_server(){
     htop -u dst
+    Printf_Green "服务器停止成功，5s后返回主菜单"
+    sleep 5
 }
 # 4.重启服务器(默认重启洞穴和主世界)
 restart_server(){
-    sh /home/dst/server_dst/bin/restart.sh
-    sh /home/dst/server_dst/bin/restart2.sh
+    bash /home/dst/server_dst/bin/restart.sh
+    bash /home/dst/server_dst/bin/restart2.sh
+    Printf_Green "重启成功,5s 后返回主菜单"
+    sleep 10
 }
 # 5.卸载饥荒服务器
 uninstall() {
     stop_server
+    userdel -rf dst
+    rm -rf /home/Steam
+    Printf_Green "卸载成功,5s 后推出脚本"
+    sleep 10
 }
 # 5.查看世界访客
 
@@ -53,8 +66,8 @@ function main_menu() {
         select option in  "安装服务器" "运行服务器" "重启服务器" "停止服务器" "管理存档" "管理mod" "任意键退出"
         do
             case $option in
-            "安装服务器") install;;
-            "运行服务器") start_server;;
+            "安装服务器") install clear;; 
+            "运行服务器") start_server clear;;
             "重启服务器") restart_server;;
             "停止服务器") stop_server;;
             "管理存档") break;;

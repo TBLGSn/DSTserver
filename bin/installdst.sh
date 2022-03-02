cd746aebeaf5430163a0b2fcb2ee620705702a1c
TBLGsn <1844760779@qq.com>
2022-03-02 20:42:27 +0800

    fix bug

--- bin/installdst.sh
+++ bin/installdst.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-#############################################################################
-############################饥荒服务器安装脚本##########################################
-#############################################################################
+###
+ # @Author: tblgsn
+ # @Date: 2022-03-02 13:20:04
+ # @Description: 饥荒服务器安装脚本
+ # @FilePath: \DSTserver\bin\installdst.sh
+### 
+
 Printf_RED(){
     echo -e "\033[41;37m $* \033[0m"
 }
 Printf_Green(){
     echo -e "\033[42;37m $* \033[0m"
 }
+INSTALL_DIR="/home/dst" # 安装目录
+
+DONOTSTARVE_DIR="$INSTALL_DIR/Klei/DoNotStarveTogether" # 存档位置
+SERVER_DST = "/home/dst/server_dst" # 脚本位置
+SERVER_DST_BIN = "/home/dst/server_dst/bin" # 脚本位置
+
 #############################################################################
 ###############################MAIN##########################################
 #############################################################################
@@ -23,49 +33,58 @@ fi
 # 2. 安装运行依赖
 Printf_Green "开始安装依赖"
 apt-get update
-echo y | apt-get install lib32gcc1
-echo y | dpkg --add-architecture i386 
-echo y | apt-get install lib32gcc1 
-echo y | apt-get install lib32stdc++6  
-echo y | apt-get install libgcc1  
-echo y | apt-get install libcurl4-gnutls-dev:i386 
-echo y | apt-get install screen 
-echo y | apt-get install htop  # 停止服务器时使用
-echo y | apt-get install git
-# 4. 添加用户(/home/dst 密码为123456)
-useradd -p -123456 -s /bin/bash -ou 0 -g 0 -d /home/dst -m dst 
-su - dst
+apt-get install -y lib32gcc1
+dpkg --add-architecture i386  
+apt-get install -y lib32stdc++6  
+apt-get install -y libcurl4-gnutls-dev:i386 
+apt-get install -y screen 
+apt-get install -y htop  # 停止服务器时使用
+apt-get install -y git
+
+
+# 3. 添加用户(/home/dst 密码为123456)
+useradd -p -123456 -s /bin/bash -ou 0 -g 0 -d $INSTALL_DIR -m dst 
 Printf_Green "添加用户成功"
-# 5.登陆用户，并下载SteamCMD
+
+# 4.登陆用户，并下载SteamCMD
 Printf_Green "下载SteamCMD"
-cd /home/dst || Printf_RED "发生错误"#默认安装在/home/dst目录下
-if  test mkdir DSTserver ;then 
-    cd DSTserver ||  Printf_RED "进入DSTserver发生错误"
+cd $INSTALL_DIR || Printf_RED "发生错误"#默认安装在/home/dst目录下
+
+if [ ! -f steamcmd_linux.tar.gz ]
+then
+	wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+	tar -xvzf steamcmd_linux.tar.gz
 fi
-wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-tar -xvzf steamcmd_linux.tar.gz
-mkdir server_dst
-./steamcmd.sh +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 
-sleep 10
-# 7.在 server_dst 目录下新建脚本
+mkdir -p server_dst
+./steamcmd.sh +force_install_dir /home/dst/server_dst  +login anonymous +app_update 343050 validate +quit
+
+# 5.在 server_dst 目录下新建脚本
 Printf_Green "在server_dst目录下新建脚本"
-cd server_dst/bin || Printf_RED "进入server_dst/bin发生错误"
 git clone git://github.com/TBLGSn/DSTserver.git
+mv DSTserver/bin/* server_dst/bin
+# rm -rf DSTserver
+cd /home/dst/server_dst/bin || Printf_RED "进入server_dst/bin发生错误"
+chmod u+x *
 
-mv DSTserver/bin/* .
-chmod u+x restart2.sh restart.sh start.sh start2.sh update.sh real_start.sh menu.sh
-# 8.运行脚本,这会这 /home/dst/Klei/DoNotStarveTogether 下生成名为“Cluster_1”的存档
+# 7.运行脚本,这会这 /home/dst/Klei/DoNotStarveTogether 下生成名为“Cluster_1”的存档
 ./real_start.sh
-#6. 移动令牌，这里用的是我的令牌
-mv -f DSTserver/Cluster_1/cluster_token.txt /home/dst/Klei/DoNotStarveTogether/Cluster_1/
-# 7.世界配置文件
-mv -f DSTserver/Cluster_1/Master/* /home/dst/Klei/DoNotStarveTogether/Cluster_1/Master/
-mv -f DSTserver/Cluster_1/Caves/* /home/dst/Klei/DoNotStarveTogether/Cluster_1/Caves/
-mv -f DSTserver/Cluster_1/cluster.ini /home/dst/Klei/DoNotStarveTogether/Cluster_1/
-# 8.配置文件只包括设置中的某些设置
+
+# 8.添加令牌
+echo "请输入你的token( Server Token )："
+read token
+cat > $DONOTSTARVE_DIR/Cluster_1/cluster_token.txt <<EOF
+$token
+EOF
+
+# 9.世界配置文件
+mv -f $INSTALL_DIR/DSTserver/Cluster_1/Master/* /home/dst/Klei/DoNotStarveTogether/Cluster_1/Master/
+mv -f $INSTALL_DIRD/STserver/Cluster_1/Caves/* /home/dst/Klei/DoNotStarveTogether/Cluster_1/Caves/
+mv -f $INSTALL_DIR/DSTserver/Cluster_1/cluster.ini /home/dst/Klei/DoNotStarveTogether/Cluster_1/
+
+# 10.配置文件只包括设置中的某些设置
 Printf_Green "删除多余文件"
-rm -rf DSTserver/ # 删除库
+rm -rf $INSTALL_DIR/DSTserver # 删除库
 Printf_Green "安装成功，请启动服务器"
 sleep 10
 

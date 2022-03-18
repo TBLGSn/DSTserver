f8d20c13f9750f36afd3fba8c700bdb7dffa6df4
tblgsn <1844760779@qq.com>
2022-03-18 19:46:30 +0800

    添加mod功能

--- bin/menu.sh
+++ bin/menu.sh
@@ -13,28 +13,48 @@ install() {
 }
 # 1. 当前服务器状态（测试 所有服务器）
 verity_status(){
-    tail -f "$DONOTSTARVE_DIR_CLUSTER_MASTER/server_log.txt"
-    tail -f "$DONOTSTARVE_DIR_CLUSTER_CAVES/server_log.txt"
+    Master_status=$(ps -ef | grep "sh start_Master.sh" | grep -v grep | awk '{print $2}')
+    Caves_status=$(ps -ef  | grep "sh start_Caves.sh"  | grep -v grep | awk '{print $2}')
+    if [ -n "$Master_status" ]; then
+        Printf_Green "饥荒地面世界正在运行"
+    else
+        Printf_RED "饥荒地面世界已停止"
+
+    fi
+    if [ -n "$Caves_status" ]; then
+        Printf_Green "饥荒地下世界正在运行"
+    else 
+        Printf_RED "饥荒地下世界已停止"
+    fi 
 }
 # 2. 更新任务等
 # TODO每天6点准时更新 设置定时
 update() {
     cd "$SERVER_DST_BIN" || Printf_RED "设置定时任务出错"
 }
-Printf_Green(){
-    echo -e "\033[42;37m $* \033[0m"
-}
+
 # 3. 启动服务器
 start_server() {
-    cd $SERVER_DST_BIN || Printf_RED "错误"
-    nohup ./start_Master.sh  >/dev/null 2>&1 &
-    nohup ./start_Caves.sh  >/dev/null 2>&1 &
-    Printf_Green "启动成功,5s 后返回主菜单"
-    sleep 5
+    # cd $SERVER_DST_BIN || Printf_RED "错误"
+    # nohup ./start_Master.sh  >/dev/null 2>&1 &
+    # nohup ./start_Caves.sh  >/dev/null 2>&1 &
+    restart_server
+    # Printf_Green "启动成功,3s 后返回主菜单"
+    sleep 3
 }
 # 3. 停止服务器
 stop_server(){
-    htop -u dst
+    Master_id=$(ps -ef | grep "sh start_Master.sh" | grep -v grep | awk '{print $2}')
+    Caves_id=$(ps -ef  | grep "sh start_Caves.sh"  | grep -v grep | awk '{print $2}')
+    
+    if [ -n "$Master_id" ];then
+          kill -9 $Master_id
+    fi
+    
+    if [ -n "$Caves_id" ];then
+        kill -9 $Caves_id
+    fi
+    # htop -u dst
     Printf_Green "服务器停止成功，5s后返回主菜单"
     sleep 5
 }
@@ -42,15 +62,15 @@ stop_server(){
 restart_server(){
     bash "$SERVER_DST_BIN/restart_Master.sh"
     bash "$SERVER_DST_BIN/restart_Caves.sh"
-    Printf_Green "重启成功,5s 后返回主菜单"
-    sleep 10
+    Printf_Green "启动成功,5s 后返回主菜单"
+    sleep 5
 }
 # 5.卸载饥荒服务器
 uninstall() {
     stop_server
     userdel -rf dst
     rm -rf /home/Steam
-    Printf_Green "卸载成功,5s 后推出脚本"
+    Printf_Green "卸载成功,5s 后退出脚本"
     sleep 10
 }
 # 5.查看世界访客
@@ -59,43 +79,81 @@ uninstall() {
 # 7.添加mods
 addmods(){
     echo '请输入需要的 modid（ 各id间使用,分隔 eg. 374550642,378160973,375850593,458587300,375859599 ） :'
-    read modid_list
+    read  modid_list
     echo ' return {' > "$DONOTSTARVE_DIR_CLUSTER_MASTER/modoverrides.lua"
     echo ' return {' > "$DONOTSTARVE_DIR_CLUSTER_CAVES/modoverrides.lua"
     if [[ $modid_list != '' ]]; then
         IFS=',' mod_list=($modid_list)
         for mod_id in ${mod_list[@]}; do
-            echo "ServerModSetup(\"$mod_id\")" >> /home/dst/server_dst/mods/dedicated_server_mods_setup.lua
-            echo "[\"workshop-$mod_id\"] = { enabled = true }," >> /home/dst/Klei/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
-            echo "[\"workshop-$mod_id\"] = { enabled = true }," >> /home/dst/Klei/DoNotStarveTogether/Cluster_1/Caves/modoverrides.lua
+            echo "ServerModSetup(\"$mod_id\")" >> "$SERVER_DST/mods/dedicated_server_mods_setup.lua"
+            echo "[\"workshop-$mod_id\"] = { enabled = true }," >> "$DONOTSTARVE_DIR_CLUSTER_MASTER/modoverrides.lua"
+            echo "[\"workshop-$mod_id\"] = { enabled = true }," >> "$DONOTSTARVE_DIR_CLUSTER_CAVES/modoverrides.lua"
         done
     fi
-    echo ' }' >> /home/dst/Klei/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
-    echo ' }' >> /home/dst/Klei/DoNotStarveTogether/Cluster_1/Caves/modoverrides.lua
+    echo ' }' >> "$DONOTSTARVE_DIR_CLUSTER_MASTER/modoverrides.lua"
+    echo ' }' >> "$DONOTSTARVE_DIR_CLUSTER_CAVES/modoverrides.lua"
+}
+
+
+deletemods(){
+    echo  "输入删除的mods号"
+    read -r delete_name
+    cat "$SERVER_DST/mods/dedicated_server_mods_setup.lua" | grep -v "ServerModSetup(\"$delete_name\")" > temp.lua
+    touch temp.lua
+    cat temp.lua > a.lua
+    rm -rf temp.lua
+}
+showmods(){
+    cat "$SERVER_DST/mods/dedicated_server_mods_setup.lua" | while read line
+    do
+        id=$(echo "$line" | grep  "^ServerModSetup(\"[0-9]*\")" | awk -F '\)' '{print $1}'  | tr -cd [0-9])
+        if [  -n "$id" ];then
+            echo "$id"
+        fi
+    done
+}
+mods(){
+    while true
+
+    do    
+        echo   "Mods 管理模块"
+        echo -en "1)添加mods"
+        echo -e "\t2)删除mods"
+        echo -en  "3)显示所有mod"
+        echo -e  "\t4)退出mods管理模块"
+        echo -en "请输入你的选项:"
+        read -r  option    
+        case $option in
+        1) addmods;; 
+        2) deletemods;;
+        3) showmods;;
+        4) break;;
+        *) ;;
+        esac
+    done
 }
 # 0. 主菜单
 
 function main_menu() {
-    while  'true' 
+    while  true 
     do
         clear
         PS3="输入选项(其他任意键退出):"
-        select option in  "安装服务器" "运行服务器" "重启服务器" "停止服务器" "管理存档" "管理mod"  "检测状态" "任意键退出"
+        select option in  "安装服务器" "运行服务器" "重启服务器" "停止服务器" "管理存档" "管理mod" "检查服务器状态"
         do
             case $option in
-            "安装服务器") install clear;;
-            "运行服务器") start_server clear;;
+            "安装服务器") install ;;
+            "运行服务器") start_server ;;
             "重启服务器") restart_server;;
             "停止服务器") stop_server;;
-            "管理存档") break;;
-            "管理mod") addmods;
-            "检测状态"） verity_status;;
-
-            *)
-                clear
-                exit
+            "管理存档") echo "管理存档";;
+            "管理mod") mods;;
+            "检查服务器状态") verity_status;;
+            *) exit;;
             esac
         done
     done
 }
+
+
 main_menu
